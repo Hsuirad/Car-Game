af40428a46b771e58197bd97c54b466414d7979b
Dariush Aligholizadeh <daligholizadeh@yahoo.com>
2019-05-07 21:08:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var canvas = document.getElementById('canvas');
-//let canvas2 = document.getElementById('canvas2');
 var c = canvas.getContext('2d');
-//let c2 = canvas2.getContext('2d');
 let width = window.innerWidth;
 let height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
-//canvas2.width = width - (0.1 * window.innerWidth);
-//canvas2.height = height - (0.1 * window.innerHeight);
 let keyCode;
 let keys = [];
 
@@ -24,7 +20,7 @@ document.addEventListener('keydown', function(){
     keys[keycode] = true;
 });
 
-window.addEventListener('keyup', function(){
+document.addEventListener('keyup', function(){
     keycode = event.which || event.keyCode;
     keys[keycode] = false;
 });
@@ -37,7 +33,6 @@ let drawrect = (x, y, w, h, d) => {
     c.rotate(d * Math.PI / 180);
     c.fillStyle = "red";
     c.fillRect(-w / 2, -h / 2 , w, h);
-    //c.fill();
     c.restore();
 }
 
@@ -59,23 +54,7 @@ let textcolor = 'white';
 
 /*************************************************************************
 
-
-
-
-
-
-
-
-    TO DETECT THE COLLISIONS JUST USE THE FOUR CORNERS OF THE CAR AND
-    USE GET IMAGE DATA TO SEE IF THERE WHITE IN FRONT AND THEN STOP
-                           THE CAR FROM MOVING 
-
-
-
-
-
-
-
+          TO DETECT THE COLLISIONS JUST USE THE DISTANCE FORMULA
 
  *************************************************************************/
 
@@ -90,7 +69,7 @@ let play_game = () => {
         speed < -8 ? speed = -8 : speed = speed;
         moving = true;
     }
-    if(keys[65] || keys[37]){ 
+    if(keys[65] || keys[37]){ //left
         anglemove > 7 ? anglemove = 7 : anglemove = anglemove;
         anglemove += angleacc;
         degrees -= anglemove;
@@ -114,25 +93,26 @@ let play_game = () => {
     }
     /**************************************
      
-            THIS GON BE DA COLLISION
+             COLLISION DETECTION
      
     ***************************************/
     // distance formula -> d = Math.sqrt(Math.pow(somepointoncar - width / 2, 2) + same for y)
     dist = Math.sqrt(Math.pow(width / 2 - x, 2) + Math.pow(height / 2 - y, 2));
     console.log(dist);
-    //inner circ = width * 0.325
-    //outer circ = width * 0.4
+    //inner circ -> width * 0.325
+    //outer circ -> width * 0.4
     if(dist >= (width > height ? height * 0.25 : width * 0.25) && dist <= (width > height ? height * 0.4 : width * 0.4)){
-        console.log('its working');
         textcolor = 'white';
     }
     else{
         score = 0;
         textcolor = 'red';
     }
-
-
-    // end collision detection
+    /**********************************
+    
+         end collision detection
+    
+    **********************************/
     x += speed * Math.cos(degrees * Math.PI / 180);
     y += speed * Math.sin(degrees * Math.PI / 180);
     drawrect(x, y, w, h, degrees);
@@ -172,27 +152,3 @@ function scoreboard(){
 }
 
 animate();
-
-// function drawRotatedRect(x,y,width,height,degrees){
-//     c.clearRect(0, 0, 1000, 1000)
-
-//     // first save the untranslated/unrotated context
-//     c.save();
-
-//     c.beginPath();
-//     // move the rotation point to the center of the rect
-//     c.translate( x+width/2, y+height/2 );
-//     // rotate the rect
-//     c.rotate(degrees*Math.PI/180);
-
-//     // draw the rect on the transformed context
-//     // Note: after transforming [0,0] is visually [x,y]
-//     //       so the rect needs to be offset accordingly when drawn
-//     c.rect( -width/2, -height/2, width,height);
-
-//     c.fillStyle="rgba(100, 100, 100, 1)";
-//     c.fill();
-
-//     // restore the context to its untranslated/unrotated state
-//     c.restore();
-// }
